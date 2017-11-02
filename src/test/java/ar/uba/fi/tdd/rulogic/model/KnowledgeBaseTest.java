907c326959348c8a1a2ef9458a9832cf476f1157
Marco Fleres <mafleres@gmail.com>
2017-11-01 22:25:17 -0300

    Agregados test sobre facts.

--- src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
+++ src/test/java/ar/uba/fi/tdd/rulogic/model/KnowledgeBaseTest.java
@@ -1,5 +1,7 @@
 package ar.uba.fi.tdd.rulogic.model;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import org.junit.Assert;
@@ -25,11 +27,48 @@ public class KnowledgeBaseTest {
 	@Test
 	public void test() {
 
-		Assert.assertFalse(this.knowledgeBase.answer("varon (javier)."));
-		Assert.assertTrue(this.knowledgeBase.answer("varon (pepe)."));
-		Assert.assertFalse(this.knowledgeBase.answer("varon (maria)."));
-		Assert.assertTrue(this.knowledgeBase.answer("mujer (maria)."));
+		assertFalse(this.knowledgeBase.answer("varon (javier)."));
+		assertTrue(this.knowledgeBase.answer("varon (pepe)."));
+		assertFalse(this.knowledgeBase.answer("varon (maria)."));
+		assertTrue(this.knowledgeBase.answer("mujer (maria)."));
 
 	}
+
+	@Test
+	public void testFacts() {
+
+		assertTrue(this.knowledgeBase.answer("varon(juan)"));
+		assertTrue(this.knowledgeBase.answer("varon(pepe)"));
+		assertTrue(this.knowledgeBase.answer("varon(hector)"));
+		assertTrue(this.knowledgeBase.answer("varon(roberto)"));
+		assertTrue(this.knowledgeBase.answer("varon(alejandro)"));
+		assertTrue(this.knowledgeBase.answer("varon(nicolas)"));
+		assertTrue(this.knowledgeBase.answer("mujer(maria)"));
+		assertTrue(this.knowledgeBase.answer("mujer(cecilia)"));
+		assertTrue(this.knowledgeBase.answer("padre(juan, pepe)"));
+		assertTrue(this.knowledgeBase.answer("padre(juan, pepa)"));
+		assertTrue(this.knowledgeBase.answer("padre(hector, maria)"));
+		assertTrue(this.knowledgeBase.answer("padre(roberto, alejandro)"));
+		assertTrue(this.knowledgeBase.answer("padre(roberto, cecilia)"));
+		assertTrue(this.knowledgeBase.answer("hermano(nicolas, roberto)"));
+		assertTrue(this.knowledgeBase.answer("hermano(roberto, nicolas)"));
+
+		assertFalse(this.knowledgeBase.answer("mujer(juan)"));
+		assertFalse(this.knowledgeBase.answer("mujer(pepe)"));
+		assertFalse(this.knowledgeBase.answer("mujer(hector)"));
+		assertFalse(this.knowledgeBase.answer("mujer(roberto)"));
+		assertFalse(this.knowledgeBase.answer("mujer(alejandro)"));
+		assertFalse(this.knowledgeBase.answer("varon(maria)"));
+		assertFalse(this.knowledgeBase.answer("varon(cecilia)"));
+		assertFalse(this.knowledgeBase.answer("padre(pepe, juan)"));
+		assertFalse(this.knowledgeBase.answer("padre(pepa, juan)"));
+		assertFalse(this.knowledgeBase.answer("padre(maria, hector)"));
+
+		assertFalse(this.knowledgeBase.answer("mujer(jgobnejaogb)"));
+		assertFalse(this.knowledgeBase.answer("varon(jgobnejaogb)"));
+		assertFalse(this.knowledgeBase.answer("padre(roberto)"));
+
+	}
+
 
 }
