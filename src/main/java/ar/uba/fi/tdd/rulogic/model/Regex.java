7e1bcc530f623495ebd6106b892a6e05c58079aa
Marco Fleres <mafleres@gmail.com>
2017-11-06 02:54:18 -0300

    Se pasan test de sintaxis de Facts que no pasaban (cuando faltaban parámetros en el fact).

--- src/main/java/ar/uba/fi/tdd/rulogic/model/Regex.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/model/Regex.java
@@ -1,8 +1,9 @@
 package ar.uba.fi.tdd.rulogic.model;
 
-public abstract class Regex {
-    public static final String espacio = "\\s*";
-    public static final String nombreFuncion = "[a-zA-Z]+";
-    public static final String parametro = "[a-zA-Z]+";
-    public static final String parametros = "(?:" + espacio + parametro + espacio + ",?)*";
+abstract class Regex {
+    static final String espacio = "\\s*";
+    static final String nombreFuncion = "[a-zA-Z]+";
+    static final String parametro = "[a-zA-Z]+";
+    /** Al menos un parametro, seguido de "," + otro parametro cualquier cantidad de veces */
+    static final String parametros = "(?:" + espacio + parametro + espacio + ")(?:,?" + espacio + parametro + espacio + ")*";
 }
